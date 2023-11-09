442ead9a0d05fcc77492459d731f9144af5225a9
Michael Sam <sam.michael.h@gmail.com>
2023-11-09 17:09:54 +0900

    Made routes, controller, actions, and views

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,25 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_parms)
+    @list.save
+    redirect_to lists_path
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+
+  def list_parms
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # As a user, I can see all my movie lists
+  # As a user, I can create a movie list
+  # As a user, I can see the details of a movie list
+  resources :lists, only: [:index, :new, :create , :show] do
+    # As a user, I can bookmark a movie inside a movie list
+    resources :bookmarks, only: [:new, :create]
+  end
+  # As a user, I can destroy a bookmark
+  resources :bookmarks, only: [:destroy]
 end
